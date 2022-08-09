ba022c4f3e5e4a5a8df2e4a6a090e788cc790c4c
adamochi <honeyrumballs@gmail.com>
2022-08-09 17:08:33 +1000

    can paint with color and change line width

--- app.js
+++ app.js
@@ -1,28 +1,105 @@
-console.log("hello");
+console.log("hello world");
+const colorOptions = Array.from(
+  document.getElementsByClassName("color-option")
+);
+const randomBtn = document.getElementById("random");
+const colourSelector = document.getElementById("color-custom");
+const linwidth = document.getElementById("line-width");
 const canvasparty = document.getElementById("canvasparty");
 const greetingHeader = document.querySelector(".greeting");
 const sayhi = document.querySelector(".sayhi");
 const ctx = canvasparty.getContext("2d");
-canvasparty.height = 800;
+canvasparty.height = 500;
 canvasparty.width = 800;
 const gday = () => {
   greetingHeader.innerText = "G-day!";
 };
-
 greetingHeader.innerText = "hello";
 sayhi.addEventListener("click", gday);
 
+const colors = [
+  "#ff3838",
+  "#ffb8b8",
+  "#c56cf0",
+  "#ff9f1a",
+  "#fff200",
+  "#32ff7e",
+  "#7efff5",
+  "#18dcff",
+  "#7d5fff",
+];
+ctx.lineWidth = linwidth.value;
+let isPainting = false;
+
+const onMove = (event) => {
+  if (isPainting) {
+    ctx.lineTo(event.offsetX, event.offsetY);
+    ctx.stroke();
+    return;
+  }
+  ctx.moveTo(event.offsetX, event.offsetY);
+};
+
+const ondown = () => {
+  ctx.beginPath();
+  isPainting = true;
+  // random thing >>
+  // ctx.beginPath();
+  // const color = colors[Math.floor(Math.random() * colors.length)];
+  // ctx.strokeStyle = color;
+  // ctx.lineTo(event.offsetX, event.offsetY);
+  // ctx.stroke();
+};
+const stopDrawing = () => {
+  isPainting = false;
+};
+const onWidthChange = () => {
+  ctx.beginPath();
+  ctx.lineWidth = linwidth.value;
+};
+
+const randomColor = () => {
+  // console.log(ctx.strokeStyle);
+  let color = colors[Math.floor(Math.random() * colors.length)];
+  // console.log(color);
+  if (color === ctx.strokeStyle) {
+    randomColor();
+  } else {
+    ctx.strokeStyle = color;
+    ctx.fillStyle = color;
+    colourSelector.value = color;
+  }
+};
+const handleColorChoice = () => {
+  ctx.strokeStyle = colourSelector.value;
+};
+
+const onColorClick = (e) => {
+  let colorOpt = e.target.dataset.color;
+  ctx.strokeStyle = colorOpt;
+  ctx.fillStyle = colorOpt;
+  colourSelector.value = colorOpt;
+};
+
+canvasparty.addEventListener("mousemove", onMove);
+canvasparty.addEventListener("mousedown", ondown);
+canvasparty.addEventListener("mouseup", stopDrawing);
+canvasparty.addEventListener("mouseleave", stopDrawing);
+linwidth.addEventListener("change", onWidthChange);
+colourSelector.addEventListener("change", handleColorChoice);
+randomBtn.addEventListener("click", randomColor);
+colorOptions.forEach((color) => color.addEventListener("click", onColorClick));
 /* some functions
-ctx.strokeRect(300, 300, 200, 200);
+ctx.strokeRect(x:, y:, width, height);
 ctx.fillRect(300, 300, 5, 200);
-ctx.beginPath();
+ctx.beginPath(x,y);
 ctx.rect();
 ctx.fill();
 ctx.fillRect();
-ctx.lineTo
+ctx.lineTo(x,y)
 
-*/
 
+// Making a house and a character
 ctx.fillStyle = "palevioletred";
 ctx.fillRect(300, 300, 5, 200);
 
@@ -79,3 +156,4 @@ ctx.moveTo(500, 430);
 ctx.lineTo(480, 460);
 
 ctx.stroke();
+*/
